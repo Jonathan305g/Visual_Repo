666dcee4c9fed9c0074473c90c78d9871b3c8f9f
Jonathan <jonathangamboa305@gmail.com>
2026-04-27 19:09:56 -0500

    Nueva clase gamboa

---
+++ src/visualcl/Gamboa.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package visualcl;
+
+/**
+ *
+ * @author jonat
+ */
+public class Gamboa {
+    
+    //HOLA
+    
+}
